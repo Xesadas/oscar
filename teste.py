1612910d72500f756b7ca25df9c9845d4bfc7d2a
Xesadas <coimbrakallil@gmail.com>
2025-03-07 18:21:50 -0300

    teste importantes

--- teste.py
+++ teste.py
@@ -1,12 +1,19 @@
 import pandas as pd
 
-# Carrega o arquivo Excel
-file_path = 'p.xlsx'
-df = pd.read_excel(file_path)
+# # Carrega o arquivo Excel
+# file_path = 'p.xlsx'
+# df = pd.read_excel(file_path)
 
-# Obtém os nomes das colunas
-column_names = df.columns
+# # Obtém os nomes das colunas
+# column_names = df.columns
 
-# Printa os nomes das colunas
-for column in column_names:
-    print(column)
+# # Printa os nomes das colunas
+# for column in column_names:
+    #print(column)
+
+# print(df.dtypes)
+# print(df.head())
+df = pd.read_excel('p.xlsx', engine="openpyxl", parse_dates=["DATA"])
+df["DATA"] = pd.to_datetime(df["DATA"], format="%d/%m/%Y", errors="coerce")
+print(df["DATA"].head())
+print(df[df["DATA"].isna()])  # Mostra linhas com datas inválidas
